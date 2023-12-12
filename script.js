5869072dcb10850814bc31a84c05d780db5362eb
fehizoro rakotoarisoa <tsimbina@hotmail.com>
2023-12-12 11:49:41 +0100

    Donkey_firstcourses_JS

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log('Hello World Alleluia !');
+console.info('Hello World INFO !');
+console.error('Hello World ERROR !');
+console.warn('Hello World WARN !');
+
+// This is a comment
+
+/* This comment
+is on multiple lines */ 
+
+/**
+* This comment
+* is used for
+* documentation
+*/
+
+let x = 12.2;
+console.log(Math.ceil(x)); // 13 (arrondit à l'entier au dessus)
+console.log(Math.round(x)); // 12 (arrondit à l'entier le plus proche)
+console.log(Math.floor(x)); // 12 (arrondit à l'entier en dessous)
